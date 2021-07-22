0b09584ee18f5a5f45ef296bf7879a4016fa4d7b
Aulig <janbunk87493@gmail.com>
2021-07-22 14:41:03 +0200

    change to seethe video instead of image

--- bot.py
+++ bot.py
@@ -50,7 +50,7 @@ async def on_message(message):
     janny_replies = [
         "https://media.discordapp.net/attachments/536819938514436116/867320829816602634/Resilienz_Coping_web.jpg",
         "https://tenor.com/view/1blocked-message-gif-19278188",
-        "https://cdn.discordapp.com/attachments/536819938514436116/867745626216333362/unknown.png"]
+        "https://cdn.discordapp.com/attachments/536819938514436116/867747945796206642/seethe.mp4",]
 
     for janny_trigger in janny_triggers:
         if janny_trigger in lower_case_content:
